c72d4522cd8376a7ece87a3579766d134e247225
VitorSalzman <31069168+VitorSalzman@users.noreply.github.com>
2019-10-30 19:51:32 -0300

    ADD BuilderEndereco + DiretorEndereco
    ESTOU FAZENDO AINDA

--- Back-end/entrega3/AppResiduos/Aplicacao/factory.py
+++ Back-end/entrega3/AppResiduos/Aplicacao/factory.py
@@ -2,8 +2,32 @@ from django.contrib.auth.models import User
 from .models import *
 
 
+
+class BuilderEndereco(estado,cep,cidade,bairro,logradouro,numero,referencia):
+	def _init_(self):
+		self.end=Endereco()
+	
+	def builderAll(self)
+		self.end=Endereco(sigla=estado,cep=cep,nome_cidade=cidade,nome_bairro=bairro,logradouro=logradouro,numero=numero,referencia=referencia)
+	def builderSave(self)
+		self.end.save()
+		
+class diretorEndereco(estado,cep,cidade,bairro,logradouro,numero,referencia):
+
+    def __init__(estado,cep,cidade,bairro,logradouro,numero,referencia):
+        self.end=BuilderEndereco(estado,cep,cidade,bairro,logradouro,numero,referencia)
+		
+	
+    def builder():
+        self.end.builderAll()
+		self.end.builderSave()
+		return end
+		
+		
 def fabricaUsuario(escolha,nome,sobrenome,email,senha,estado,cep,cidade,bairro,logradouro,numero,referencia,cpf_hab_cnpj):
-    end = builderEndereco(estado,cep,cidade,bairro,logradouro,numero,referencia)
+    director=diretorEndereco(estado,cep,cidade,bairro,logradouro,numero,referencia)
+	end = director.builder()
+	
     user = builderUsuario(nome,sobrenome,email,senha)
     carteira = builderCarteira()
     
@@ -12,7 +36,7 @@ def fabricaUsuario(escolha,nome,sobrenome,email,senha,estado,cep,cidade,bairro,l
 
 
 
-
+def diretorEndereco()
 def builderCliente(usuario,carteira,endereco):
     cliente  = Cliente(usuario=usuario,carteira=carteira,endereco=endereco)
     cliente.save()
