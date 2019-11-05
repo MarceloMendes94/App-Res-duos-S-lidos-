07703a8dd9ab50fdf9d25cf34c9ab78d3029dcb5
Hellesandro <hellesandro@hotmail.com>
2019-11-05 02:08:42 -0300

    ADD: Ambiente virtual python (venv)
    Ambiente criado para manter o padrão entre versões do django entre os colaboradores do projeto

---
+++ Back-end/entrega3/AppResiduos/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Kraken\App-Res-duos-S-lidos-\Back-end\entrega3\AppResiduos\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
