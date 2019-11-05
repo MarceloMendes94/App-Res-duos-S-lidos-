0dc3b7ffa13fa367bc0cd43c1ce55b6c1e8402f1
Hellesandro <hellesandro@hotmail.com>
2019-11-05 02:09:30 -0300

    UPDATE: Atualização dos models (incompleto)

--- Back-end/entrega3/AppResiduos/Aplicacao/models.py
+++ Back-end/entrega3/AppResiduos/Aplicacao/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Referencias
+#https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Django/Models
+
+#Modelo de classe base
+#https://www.lucidchart.com/documents/edit/7f4db886-7f9f-4c77-a858-5c4e38aad54a/YGcM5DNywbTK?shared=true
+
 # INICIO da estrutura de endereço        
 class Estado(models.Model):
-    sigla = models.CharField(max_length=2)
+    #da pra usar choices aqui
+    #consulte: https://docs.djangoproject.com/en/2.1/ref/models/fields/#field-options
+    sigla = models.CharField(max_length=2, help_text='Informe a sigla do seu estado.\nExemplo: ES (Espírito Santo)')
+
     def __str__(self):
         return self.sigla
 
 class Cidade(Estado):
     nome_cidade = models.CharField(max_length=35)
+
     def __str__(self):
+
         return self.nome_cidade
 
 class Bairro(Cidade):
     nome_bairro = models.CharField(max_length=35)
+
     def __str__(self):
         return self.nome_bairro
 
@@ -22,14 +34,49 @@ class Endereco(Bairro):   # cep e estado com mascara no html
     cep        = models.CharField(max_length=8)
     numero     = models.CharField(max_length=4)
     referencia = models.TextField()
+
     def __str__(self):
         return self.cep+" "+self.numero+" "+self.nome_bairro
 # FIM da estrutura de endereço  
+
+class Pessoa(models.Model):
+    nome_completo = models.CharField(max_length=100)
+    cpf           = models.CharField(max_length=14)
+    dt_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome_completo +" "+ self.cpf
+
+class Usuario(Pessoa):
+    email = models.EmailField(max_length=50)
+    senha = models.CharField(max_length=12)
+
+    def __str__(self):
+        return "Login:" + self.email
+
+class Cliente(Usuario):
+
+    def __str__(self):
+        return "Login Cliente:" + self.email
+
+class Motorista(Usuario):
+    placa       = models.CharField(max_length=8)
+    nada_consta = models.BooleanField(None)
+
+
+class Habilitacao(models.Model):
+    numero   = models.CharField(max_length=14)
+    tipo     = models.CharField(max_length=5)
+    validade = models.DateField()
+
+    def __str__(self):
+        return "Habilitacao"+self.numero
 
 #INICIO DA CARTEIRA
 class Carteira(models.Model):
     opcoes      = (('m','Motorista'),('c','Cliente'))    
     saldo       = models.DecimalField(decimal_places=2, max_digits=8)
+
     def __str__(self):
         return str(self.saldo)
 # FIM da estrutura de carteira
@@ -41,6 +88,7 @@ class Cliente(models.Model):
     endereco = models.ForeignKey(Endereco,on_delete=models.CASCADE)
     data_nascimento = models.DateField()
     cpf = models.CharField(max_length=11)
+
     def __str__(self):
         return self.usuario.email
 # FIM da estrutura de cliente
