de74753387e846a61941c925ad248186cea51185
Serenna <serenna.ferrari@hotmail.com>
2019-11-11 23:21:42 -0300

    entrega CE
    FALHOU MEUS IRMAOS

--- AppResiduos-CE/AppResiduos/urls.py
+++ AppResiduos-CE/AppResiduos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from Aplicacao import views
 urlpatterns = [
     path('admin/' , admin.site.urls),
-    path(''       , views.index),
+    path(''       , views.index, name='home'),
     path('login/' , views.login_page),
     path('login/submit', views.login_submit),
     path('logout/', views.logout_user),
